668c9f44605acde2bc62eaf764f3a300980b3c88
SalmaShkedim <salma.shkedim@sapiens.com>
2021-11-04 13:34:24 +0200

    Day3 - Hands On - Task2

--- src/main/java/com/sapiensCourses/FirstSpring/SetterDI/MainDI.java
+++ src/main/java/com/sapiensCourses/FirstSpring/SetterDI/MainDI.java
@@ -5,20 +5,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainDI {
     public static void main(String[] args) {
-        AbstractApplicationContext context1 = new ClassPathXmlApplicationContext("SetterBeans.xml");
-        Communication communication1 = (Communication) context1.getBean("communication1");
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("SetterBeans.xml");
+
+        Communication communication1 = (Communication) context.getBean("communication1");
         communication1.communication();
 
-        AbstractApplicationContext context2 = new ClassPathXmlApplicationContext("SetterBeans.xml");
-        Communication communication2 = (Communication) context2.getBean("communication2");
+        Communication communication2 = (Communication) context.getBean("communication2");
         communication2.communication();
 
-        AbstractApplicationContext context3 = new ClassPathXmlApplicationContext("SetterBeans.xml");
-        Communication communication3 = (Communication) context3.getBean("communication3");
+        Communication communication3 = (Communication) context.getBean("communication3");
         communication3.communication();
 
-        AbstractApplicationContext context4 = new ClassPathXmlApplicationContext("SetterBeans.xml");
-        Communication communication4 = (Communication) context4.getBean("communication4");
+        Communication communication4 = (Communication) context.getBean("communication4");
         communication4.communication();
     }
 
